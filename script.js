ea228e4a0d63d9969f8d364a622ce47907e64121
J-C <jcjoneskern@gmail.com>
2017-11-06 16:11:38 -0500

    basic styles

--- script.js
+++ script.js
@@ -8,6 +8,22 @@ let i = 0;
 $("." + i).css("font-style", "italic");
 
 function increment() {
-    i++;
+    if (i < cats.length - 1) {
+        i++;
+        prev = i - 1;
+        
+        $("#currentIndex").text(i);
+        $("#currentArrVal").html("<span class='" + i + "'>" + cats[i] + "</span>");
+        $("#output").append("<br><span class='" + i + "'>" + cats[i] + "</span>");
+    }
+
+    $("." + i).css("font-style", "italic");
+    $("." + prev).css("font-style", "normal");
+}
+
+function reset () {
+    $("#output").html("<span class='0'>siamese</span>");
+    $("." + i).css("font-style", "normal");
+    i = 0;
     $("." + i).css("font-style", "italic");
 }
